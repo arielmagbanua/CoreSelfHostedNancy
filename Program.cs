8428c4712159d4287950dd57c67a4ea1f89f1409
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-02-12 13:27:53 +0800

    Added project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreSelfHostedNancy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
