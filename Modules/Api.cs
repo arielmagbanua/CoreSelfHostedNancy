fe2fe2b7a5d617e7cf7b8744a4eb5df3cc5509c8
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-02-13 09:09:32 +0800

    Added html views.

---
+++ Modules/Api.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+
+namespace CoreSelfHostedNancy.Modules
+{
+    public class Api : NancyModule
+    {
+        public Api() : base("api")
+        {
+            Get("/", (p) => {
+                return View["api.html"];
+            });
+
+            // Returning RAW json string as json response.
+            Get("/raw", (p) => {
+                string data = "{ \"foo\" : \"BAR\" }";
+
+                return Response.AsText(data, "application/json");
+            });
+        }
+    }
+}
