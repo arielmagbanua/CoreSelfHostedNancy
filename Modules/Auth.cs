cf1b0c097e60d3a70764ee77e3e9c5040b8f9fb2
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-02-13 14:18:41 +0800

    Added example of reading custom headers.

---
+++ Modules/Auth.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Nancy.ModelBinding;
+using Nancy;
+using CoreSelfHostedNancy.Models;
+using System.Linq;
+
+namespace CoreSelfHostedNancy.Modules
+{
+    public class Auth : NancyModule
+    {
+        public Auth() : base("auth")
+        {
+            // post route with custom headers
+            Post("/custom", (args) => {
+                Console.WriteLine(args);
+                Console.WriteLine(this.Request.Headers.Authorization);
+
+                string xToken = this.Request.Headers["x-token"].FirstOrDefault();
+                string xKey = this.Request.Headers["x-key"].FirstOrDefault();
+
+                var xAuth = this.BindTo(
+                        new
+                        {
+                            XToken = xToken,
+                            XKey = xKey
+                        }
+                    );
+
+                return Response.AsJson(xAuth)
+                                .WithHeader("Foo", "Bar");
+            });
+        }
+    }
+}
